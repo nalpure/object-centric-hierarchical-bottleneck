fixed calculation of disentanglement loss (consider scenario where delta is subtracted); added parameter to control learning rate of slot and projection parameters seperately; adjusted warmup and decay steps depeding on number of epochs

slot_attention/train.py
@@ -48,7 +48,8 @@ parser.add_argument('--num_epochs', default=100, type=int, help='number of epoch
 # Disentanglement parameters
 parser.add_argument('--disentangle', default=False, action='store_true', help='If true, adds disentanglement loss. Expects training data to include perturbations.')
 parser.add_argument('--latent_dim', default=None, type=int, help='If disentangle is true, specify the latent dimensionality.')
-parser.add_argument('--loss_ratio', default=100, type=float, help='Ratio of reconstruction loss to disentanglement loss. Higher values favor reconstruction loss.')
+parser.add_argument('--loss_ratio', default=100, type=int, help='Ratio of reconstruction loss to disentanglement loss. Higher values favor reconstruction loss.')
+parser.add_argument('--lr_ratio', default=0.25, type=float, help='Ratio of slot parameters learning rate to disentanglement parameters learning rate. Higher values favor reconstruction parameters.')
 parser.add_argument('--disentangle_warmup', default=20, type=int, help='Number of warmup epochs for disentanglement parameters.')
 
 args = parser.parse_args()
@@ -69,7 +70,7 @@ def main():
         warnings.warn('A validation path was specified but will not be used.')
         
     set_seed(args['seed'])
-    model, optimizer_full, optimizer_disentangle = initialize_model()
+    model, optimizer_full, optimizer_projections = initialize_model()
     
     print("Loading training data...")
     
@@ -83,7 +84,7 @@ def main():
 
     if args["disentangle"]:
         print("Warmup new disentangle parameters...")
-        train(model, optimizer_disentangle, train_dataloader, args["disentangle_warmup"], reconstruct=False, disentangle=True)
+        train(model, optimizer_projections, train_dataloader, args["disentangle_warmup"], reconstruct=False, disentangle=True)
         print(f"Training full model with ratio {args['loss_ratio']}...")
         train(model, optimizer_full, train_dataloader, args["num_epochs"], reconstruct=True, disentangle=True)
     else:
@@ -114,7 +115,7 @@ def train(model, optimizer, train_dataloader, num_epochs, reconstruct=True, dise
         for batch in train_dataloader:
             total_loss = 0
 
-            obs = batch[0].to(device)
+            obs = batch[0].to(device)   # [B, C, H, W]
             if TRAINING_NOISE:
                 obs += (torch.randint(0, 3, (1,)) > 0) * 0.5 * torch.rand((1, obs.shape[1], 1, 1)).clip(0, 1)
 
@@ -141,12 +142,16 @@ def train(model, optimizer, train_dataloader, num_epochs, reconstruct=True, dise
                 epoch_disentangle_loss += disentangle_loss.item()
                 total_loss += disentangle_loss
 
-            # Adjust the learning rate based on warmup and decay schedule
-            if current_step < args["warmup_steps"]:
-                lr = args["learning_rate"] * (current_step / args["warmup_steps"])
+            # Adjust number of warmup and decay steps if number of epochs is different from the default
+            warmup_steps = args["warmup_steps"] * (num_epochs / args["num_epochs"]) 
+            decay_steps = args["decay_steps"] * (num_epochs / args["num_epochs"])
+            
+            # Adjust the learning rate based on warmup and decay steps
+            if current_step < warmup_steps:
+                lr = args["learning_rate"] * (current_step / warmup_steps)
             else:
                 lr = args["learning_rate"]
-            lr = lr * (args["decay_rate"] ** (current_step / args["decay_steps"]))
+            lr = lr * (args["decay_rate"] ** (current_step / decay_steps))
             optimizer.param_groups[0]['lr'] = lr
 
             optimizer.zero_grad()
@@ -208,12 +213,14 @@ def disentanglement_loss(z_obs, z_perturbed, magnitudes):
     z_perturbed_expanded = z_perturbed.unsqueeze(1).unsqueeze(3)        # [B, 1, S, 1, D]
     deltas_expanded = deltas.unsqueeze(1).unsqueeze(1)                  # [B, 1, 1, D, D]
 
-    #TODO: Delta can be positive or negative, so the loss should be the minimum of the two
-    diff = z_perturbed_expanded - (z_obs_expanded + deltas_expanded)    # [B, S, S, D, D]
-    diff_norm = torch.norm(diff, dim=-1)                                # [B, S, S, D]
+    batch_loss = 1e9
 
-    losses = diff_norm.min(dim=-1).values.min(dim=-1).values.min(dim=-1).values # [B]
-    batch_loss = losses.sum()
+    for sign in [-1, 1]:
+        diff = z_perturbed_expanded - (z_obs_expanded + deltas_expanded * sign)    # [B, S, S, D, D]
+        diff_norm = torch.norm(diff, dim=-1)                                # [B, S, S, D]
+        losses = diff_norm.min(dim=-1).values.min(dim=-1).values.min(dim=-1).values # [B]
+        if losses.sum() < batch_loss:
+            batch_loss = losses.sum()
 
     return batch_loss
 
@@ -251,50 +258,54 @@ def initialize_model():
             args["small_arch"]
         )
 
+    params_all = model.parameters()
+    params_slots = []
+    params_projections = []
+
     # Load model weights from checkpoint if provided
     if args["init_ckpt"] is not None:
         print(f"Loading model weights from {args['init_ckpt']}")
         checkpoint = torch.load(args["init_ckpt"], weights_only=True)
         missing_keys, unexpected_keys = model.load_state_dict(checkpoint["model_state_dict"], strict=False)
 
+        for name, param in model.named_parameters():
+            if name in missing_keys:  # Parameters not found in the checkpoint are "new"
+                params_projections.append(param)
+            else:
+                params_slots.append(param)
+
         if unexpected_keys:
             warnings.warn("Ignoring unexpected keys:", unexpected_keys)
 
     model.to(device)
-
     model.encoder_cnn.encoder_pos.grid = model.encoder_cnn.encoder_pos.grid.to(device)
     model.decoder_cnn.decoder_pos.grid = model.decoder_cnn.decoder_pos.grid.to(device)
 
-
-    all_params = model.parameters()
-
-    params_reconstruct = []
-    params_disentangle = []
-
-    for name, param in model.named_parameters():
-        if name in missing_keys:  # Parameters not found in the checkpoint are "new"
-            params_disentangle.append(param)
-        else:
-            params_reconstruct.append(param)
-
-    optimizer = initialize_optimizer(all_params, args["learning_rate"])
-
-    if params_disentangle:
-        optimizer_new = initialize_optimizer(params_disentangle, args["learning_rate"] * 2)
+    if params_projections:
+        optimizer_projections = initialize_optimizer([
+            {'params': params_projections, 'lr': args["learning_rate"]}
+        ])
+        optimizer_full = initialize_optimizer([
+            {'params': params_projections, 'lr': args["learning_rate"]},
+            {'params': params_slots, 'lr': args["learning_rate"] * args["lr_ratio"]}
+        ])
     else:
-        optimizer_new = None
+        optimizer_full = initialize_optimizer([
+            {'params': params_all, 'lr': args["learning_rate"]}
+        ])
+        optimizer_projections = None
     
-    return model, optimizer, optimizer_new
+    return model, optimizer_full, optimizer_projections
 
 
-def initialize_optimizer(params, lr=0.0004):
+def initialize_optimizer(init_list):
     """Initialize the optimizer for the model."""
     if args["optimizer"] == "adam":
-        optimizer = optim.Adam(params, lr=lr)
+        optimizer = optim.Adam(init_list)
     elif args["optimizer"] == "rmsprop":
-        optimizer = optim.RMSprop(params, lr=lr)
+        optimizer = optim.RMSprop(init_list)
     elif args["optimizer"] == "sgd":
-        optimizer = optim.SGD(params, lr=lr)
+        optimizer = optim.SGD(init_list)
     else:
         raise ValueError("Select a valid optimizer.")
     